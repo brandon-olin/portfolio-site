2d2f51a78b2de4c7dd918a184dbc747b6d0d12eb
brandon olin <olin.brandon@gmail.com>
2020-04-08 09:05:57 -0400

    added a header and bootstrap button

--- app/javascript/components/banner.js
+++ app/javascript/components/banner.js
@@ -2,7 +2,7 @@ import Typed from 'typed.js';
 
 const loadDynamicBannerText = () => {
   new Typed('#banner-typed-name', {
-    strings: ["Brandon Olin^1000\nJunior Ruby Developer"],
+    strings: ["Brandon Olin^1000\nRuby on Rails Developer"],
     typeSpeed: 50,
     loop: false
   });
